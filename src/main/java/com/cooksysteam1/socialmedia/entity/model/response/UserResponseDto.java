f7d9e10db8cfcf6d27e4e02b765e8020541f0794
Jacob T <Jacob.m.thompson4@gmail.com>
2022-06-29 14:02:04 -0600

    Mappers were updated and are complete

--- src/main/java/com/cooksysteam1/socialmedia/entity/model/response/UserResponseDto.java
+++ src/main/java/com/cooksysteam1/socialmedia/entity/model/response/UserResponseDto.java
@@ -1,4 +1,12 @@
 package com.cooksysteam1.socialmedia.entity.model.response;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserResponseDto {
+    private String username;
 }
--- src/main/java/com/cooksysteam1/socialmedia/mapper/ProfileMapper.java
+++ src/main/java/com/cooksysteam1/socialmedia/mapper/ProfileMapper.java
@@ -1,7 +1,20 @@
 package com.cooksysteam1.socialmedia.mapper;
 
+import com.cooksysteam1.socialmedia.entity.model.request.ProfileRequestDto;
+import com.cooksysteam1.socialmedia.entity.model.response.ProfileResponseDto;
+import com.cooksysteam1.socialmedia.entity.resource.Profile;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProfileMapper {
+
+    Profile requestToEntity(ProfileRequestDto profileRequestDto);
+
+    List<Profile> requestsToEntities(List<ProfileRequestDto> profileRequestDtos);
+
+    ProfileResponseDto entityToResponse(Profile profile);
+
+    List<ProfileResponseDto> entitiesToResponses(List<Profile> profiles);
 }
