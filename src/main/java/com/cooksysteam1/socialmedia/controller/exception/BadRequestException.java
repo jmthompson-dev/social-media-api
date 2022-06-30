7ef5b396ffa6702c99bb128f9d0ff25dc88fd1da
idreesammar <66299958+idreesammar@users.noreply.github.com>
2022-06-29 17:24:31 -0700

    added to Exception classes

--- src/main/java/com/cooksysteam1/socialmedia/controller/exception/BadRequestException.java
+++ src/main/java/com/cooksysteam1/socialmedia/controller/exception/BadRequestException.java
@@ -1,5 +1,13 @@
 package com.cooksysteam1.socialmedia.controller.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
 public class BadRequestException extends RuntimeException{
     private final static long serialVersionUID = -4204927909913047640L;
+    
+    private String message;
 }
