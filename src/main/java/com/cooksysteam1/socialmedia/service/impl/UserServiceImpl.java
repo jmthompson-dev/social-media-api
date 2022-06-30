f67f41faeb46791b9a4cb6d7d6b5d9998d3f37b0
Jacob T <Jacob.m.thompson4@gmail.com>
2022-06-30 15:51:46 -0600

    Refactored Validations

--- src/main/java/com/cooksysteam1/socialmedia/service/impl/UserServiceImpl.java
+++ src/main/java/com/cooksysteam1/socialmedia/service/impl/UserServiceImpl.java
@@ -70,6 +70,14 @@ public class UserServiceImpl implements UserService {
         return userMapper.entityToResponse(userRepository.saveAndFlush(user));
     }
 
+    @Override
+    public UserResponseDto getUserByUsername(String username) {
+        validateUsername(username);
+        Optional<User> optionalUser = userRepository.findUserByCredentials_UsernameAndDeletedFalse(username);
+        User user = validateOptionalAndReturnsUser(optionalUser); // optionalUser.get();
+        return userMapper.entityToResponse(user);
+    }
+
     private void validateUserCredentialsAgainstCredentialsDto(Credentials userCredentials, CredentialsDto credentialsDto) {
         if (!userCredentials.equals(credentialsMapper.requestToEntity(credentialsDto))) throw new NotAuthorizedException("Invalid credentials. Expected credentials to ,atch but was false.");
     }
@@ -120,24 +128,5 @@ public class UserServiceImpl implements UserService {
 		if (user.isDeleted() || user == null) {
 			throw new NotFoundException("This user cannot be found.");
 		}
-	}
-	
-	private void validateUsername (String username) {
-		if (!userRepository.existsUserByCredentials_Username(username)) {
-			throw new NotFoundException("No user found with username: " + username);
-		}
-	}
-	
-	@Override
-	public UserResponseDto getUserByUsername(String username) {
-		validateUsername(username);
-		
-		Optional<User> optionalUser = userRepository.findUserByCredentials_Username(username);
-		validateOptionalUser(optionalUser);
-
-		User user = optionalUser.get();
-		validateUser(user);
-		
-		return userMapper.entityToResponse(user);
 	}
 }
