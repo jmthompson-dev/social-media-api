3ee70203a5350df26e368c0f6823c92d86f15d3a
Jacob T <Jacob.m.thompson4@gmail.com>
2022-06-30 11:06:03 -0600

    Resolved confict

--- src/main/java/com/cooksysteam1/socialmedia/controller/UserController.java
+++ src/main/java/com/cooksysteam1/socialmedia/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/users")
 @RestController
 public class UserController {
 
