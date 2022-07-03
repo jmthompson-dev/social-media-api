2385b2413422cbcaa83558a858db3375645e267d
Jacob <Jacob.m.thompson4@gmail.com>
2022-07-03 01:41:52 -0600

    Refactor after merge conflict

--- src/main/java/com/cooksysteam1/socialmedia/repository/HashtagRepository.java
+++ src/main/java/com/cooksysteam1/socialmedia/repository/HashtagRepository.java
@@ -4,11 +4,11 @@ import com.cooksysteam1.socialmedia.entity.Hashtag;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 @Repository
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
 	
 	boolean existsByLabel(String label);
-  
+
+	Optional<Hashtag> findHashtagByLabelIgnoreCase(String label);
 }
--- src/main/java/com/cooksysteam1/socialmedia/service/impl/ValidateServiceImpl.java
+++ src/main/java/com/cooksysteam1/socialmedia/service/impl/ValidateServiceImpl.java
@@ -1,15 +1,16 @@
 package com.cooksysteam1.socialmedia.service.impl;
 
 
+import com.cooksysteam1.socialmedia.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import com.cooksysteam1.socialmedia.repository.HashtagRepository;
 import com.cooksysteam1.socialmedia.service.ValidateService;
 import lombok.RequiredArgsConstructor;
-
 @Service
 @RequiredArgsConstructor
 public class ValidateServiceImpl implements ValidateService {
 	
+    private final HashtagRepository hashtagRepository;
     private final UserRepository userRepository;
 
     @Override
